777578273389bd1ed08ba5fa37d4f2c37821d64c
Thomas Raugust <thomas.raugust@volkswagen.de>
2020-04-17 11:39:52 +0200

    update trufflehog default options

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,6 @@ query="$args $githubRepo" # Build args query with repository url
 
 
 echo Running trufflehog3 $query
-trufflehog3 $query
 echo "::set-output name=numWarnings::strawberry"
+trufflehog3 $query
 
